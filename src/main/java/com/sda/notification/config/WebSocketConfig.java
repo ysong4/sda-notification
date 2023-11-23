a30b9d5d7a4f87982e92ffe9db834cd58e1353e2
Yang Song <yangson2@andrew.cmu.edu>
2023-11-22 22:55:46 -0800

    enable websocket endpoint

--- src/main/java/com/sda/notification/config/WebSocketConfig.java
+++ src/main/java/com/sda/notification/config/WebSocketConfig.java
@@ -12,6 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
